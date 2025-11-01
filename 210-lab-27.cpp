deec2c5fb95dd85b55d50fc67568df0a9f1df44d
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-01 14:31:16 -0700

    wrote incrase and decrase functions, pasuing session

--- 210-lab-27.cpp
+++ 210-lab-27.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-int printMenu(); 
+int menu_select(); 
 void increaseFriendship(map<string, tuple<int, string, string>>&);
 void decreaseFriendship(map<string, tuple<int, string, string>>&);
 void searchFriendship(map<string, tuple<int, string, string>>&);
@@ -19,53 +19,66 @@ int main() {
     villagerInfo["Raymond"] = {10, "Wolf", "Hubba hubba!"};
     villagerInfo.insert({"Marshal", {8, "Cat", "Nice fit"}});
 
-    // access the map using a range-based for loop
-    cout << "Villagers and their favorite colors (range-based for loop):" << endl;
-    for (auto pair : villagerInfo) {
-        cout << pair.first << "[ ";
-        cout << get<0>(pair.second) << " "
-             << get<1>(pair.second) << " "
-             << get<2>(pair.second) << " ]";  
-        cout << endl;
+    int menuChoice = menu_select(); 
+    while (menuChoice != 4) { 
+        if(menuChoice == 1) { 
+            increaseFriendship(villagerInfo);
+        } else if (menuChoice == 2) { 
+            decreaseFriendship(villagerInfo);
+        } else if (menuChoice == 3) { 
+            searchFriendship(villagerInfo); 
+        }
+
+
+        // access the map using a range-based for loop
+        cout << endl << "Villagers and their favorite colors (range-based for loop):" << endl;
+        for (auto pair : villagerInfo) {
+            cout << pair.first << "[ ";
+            cout << get<0>(pair.second) << " "
+                << get<1>(pair.second) << " "
+                << get<2>(pair.second) << " ]";  
+            cout << endl;
+        }
+        menuChoice = menu_select(); 
+        /*
+        // access the map using iterators
+        cout << "\nVillagers and their favorite colors (iterators):" << endl;
+        for (map<string, tuple<int, string, string>>::iterator it = villagerInfo.begin(); 
+                                                it != villagerInfo.end(); ++it) {
+            cout << it->first << "[ ";
+            cout << get<0>(it->second) << " "
+                << get<1>(it->second) << " "
+                << get<2>(it->second) << " ]";
+            cout << endl;
+        }
+
+        // delete an element
+        villagerInfo.erase("Raymond");
+
+        // search for an element using .find() to avoid errors
+        string searchKey = "Audie";
+        auto it = villagerInfo.find(searchKey);
+        if (it != villagerInfo.end()) {  // the iterator points to beyond the end of the map
+                                        // if searchKey is not found
+            cout << "\nFound " << searchKey << ": ";
+            cout << it->first << "[ ";
+            cout << get<0>(it->second) << " "
+                << get<1>(it->second) << " "
+                << get<2>(it->second) << " ]";
+            cout << endl;
+        } else
+            cout << endl << searchKey << " not found." << endl;
+
+        // report size, clear, report size again to confirm map operations
+        cout << "\nSize before clear: " << villagerInfo.size() << endl;
+        villagerInfo.clear();
+        cout << "Size after clear: " << villagerInfo.size() << endl;
+        */
     }
-
-    // access the map using iterators
-    cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, tuple<int, string, string>>::iterator it = villagerInfo.begin(); 
-                                               it != villagerInfo.end(); ++it) {
-        cout << it->first << "[ ";
-        cout << get<0>(it->second) << " "
-             << get<1>(it->second) << " "
-             << get<2>(it->second) << " ]";
-        cout << endl;
-    }
-
-    // delete an element
-    villagerInfo.erase("Raymond");
-
-    // search for an element using .find() to avoid errors
-    string searchKey = "Audie";
-    auto it = villagerInfo.find(searchKey);
-    if (it != villagerInfo.end()) {  // the iterator points to beyond the end of the map
-                                       // if searchKey is not found
-        cout << "\nFound " << searchKey << ": ";
-        cout << it->first << "[ ";
-        cout << get<0>(it->second) << " "
-             << get<1>(it->second) << " "
-             << get<2>(it->second) << " ]";
-        cout << endl;
-    } else
-        cout << endl << searchKey << " not found." << endl;
-
-    // report size, clear, report size again to confirm map operations
-    cout << "\nSize before clear: " << villagerInfo.size() << endl;
-    villagerInfo.clear();
-    cout << "Size after clear: " << villagerInfo.size() << endl;
-
     return 0;
 }
 
-int printMenu() {
+int menu_select() {
     int decision; 
     cout << "1. Increase Friendship" << endl
          << "2. Decrease Friendship" << endl
@@ -77,13 +90,13 @@ int printMenu() {
 }
 
 void increaseFriendship(map<string, tuple<int, string, string>>& villagerInfo) { 
-    for (auto pair : villagerInfo) {
+    for (auto& pair : villagerInfo) {
         get<0>(pair.second) += 1;
     }
 }
 
 void decreaseFriendship(map<string, tuple<int, string, string>>& villagerInfo) { 
-    for (auto pair : villagerInfo) {
+    for (auto& pair : villagerInfo) {
         if(get<0>(pair.second) >= 1) { 
             get<0>(pair.second) -= 1; 
         } 
