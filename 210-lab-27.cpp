ee2583b2a2044e6e7663110f1fdb46444195b158
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-11-01 15:07:05 -0700

    Milestone 5

--- 210-lab-27.cpp
+++ 210-lab-27.cpp
@@ -41,49 +41,14 @@ int main() {
         cout << endl << "Villagers details: " << endl;
         for (auto pair : villagerInfo) {
             cout << pair.first << " [";
-            cout << get<0>(pair.second) << " "
-                << get<1>(pair.second) << " "
+            cout << get<0>(pair.second) << ", "
+                << get<1>(pair.second) << ", "
                 << get<2>(pair.second) << "]";  
             cout << endl;
         }
 
         cout << endl;
         menuChoice = menu_select(); 
-         
-        /*
-        // access the map using iterators
-        cout << "\nVillagers and their favorite colors (iterators):" << endl;
-        for (map<string, tuple<int, string, string>>::iterator it = villagerInfo.begin(); 
-                                                it != villagerInfo.end(); ++it) {
-            cout << it->first << "[ ";
-            cout << get<0>(it->second) << " "
-                << get<1>(it->second) << " "
-                << get<2>(it->second) << " ]";
-            cout << endl;
-        }
-
-        // delete an element
-        villagerInfo.erase("Raymond");
-
-        // search for an element using .find() to avoid errors
-        string searchKey = "Audie";
-        auto it = villagerInfo.find(searchKey);
-        if (it != villagerInfo.end()) {  // the iterator points to beyond the end of the map
-                                        // if searchKey is not found
-            cout << "\nFound " << searchKey << ": ";
-            cout << it->first << "[ ";
-            cout << get<0>(it->second) << " "
-                << get<1>(it->second) << " "
-                << get<2>(it->second) << " ]";
-            cout << endl;
-        } else
-            cout << endl << searchKey << " not found." << endl;
-
-        // report size, clear, report size again to confirm map operations
-        cout << "\nSize before clear: " << villagerInfo.size() << endl;
-        villagerInfo.clear();
-        cout << "Size after clear: " << villagerInfo.size() << endl;
-        */
     }
     return 0;
 }
@@ -142,6 +107,7 @@ void addVillager(map<string, tuple<int, string, string>>& villagerInfo) {
     cout << "Species: "; 
     cin >> species; 
     cout << "Catchprase: "; 
+    cin.ignore();
     getline(cin, catchphrase);
 
     villagerInfo.insert({name, {friendshipLevel, species, catchphrase}});
